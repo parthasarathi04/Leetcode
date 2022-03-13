7d8441e66ebc89d1bee19674b434eedd29c3c98c
Partha Sarathi Bhunia <63489674+parthasarathi04@users.noreply.github.com>
2022-03-13 07:57:48 +0530

    Create 20   Valid Parentheses.java

---
+++ 20   Valid Parentheses.java
@@ -0,0 +1,35 @@
+class Solution {
+
+    public boolean isValid(String s) {
+
+        Stack<Character> st = new Stack<>(); 
+
+        for(char c:s.toCharArray()){
+
+            if(c=='('||c=='{'||c=='['){
+
+                st.push(c);
+
+            }
+
+            else{
+
+                if(st.empty()) return false;
+
+                if(c==')'&&st.peek()!='(') return false; 
+
+                if(c=='}'&&st.peek()!='{') return false;
+
+                if(c==']'&&st.peek()!='[') return false;
+
+                st.pop();
+
+            }
+
+         }
+
+        return st.empty();
+
+    }
+
+}
